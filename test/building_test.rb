b5a506c1f3b9252622e511fa73ece7159dfdd6f3
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-02-17 09:35:08 -0700

    Add method add_unit; test passing.

--- test/building_test.rb
+++ test/building_test.rb
@@ -5,8 +5,24 @@ require './lib/apartment'
 require './lib/building'
 
 class BuildingTest < Minitest::Test
+  def setup
+    @building = Building.new
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    @unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    @unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    @renter1 = Renter.new("Spencer")
+  end
+
   def test_it_exists
-    building = Building.new
-  assert_instance_of Building, building
+    assert_instance_of Building, @building
+  end
+
+  def test_it_can_add_units
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @building.add_unit(@unit3)
+    assert_equal [@unit1, @unit2, @unit3], @building.units
+    # assert_equal [], @building.rented_units
   end
 end
